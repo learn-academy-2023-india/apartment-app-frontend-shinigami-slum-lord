7c43921035c96774842126038b4b8eb539c4dfac
Keoni <joonhyuk5972@gmail.com>
2024-02-21 11:25:08 -1000

    commented out code

--- src/__test__/Header.test.js
+++ src/__test__/Header.test.js
@@ -9,7 +9,7 @@ test("renders header navigation links", () => {
       <Header />
     </BrowserRouter>
   );
-  
+
   const homeLink = screen.getByRole('link', {
     name: /home/i
   })
@@ -20,25 +20,8 @@ test("renders header navigation links", () => {
 
   expect(homeLink).toBeInTheDocument();
   expect(viewApartmentsLink).toBeInTheDocument();
-  // expect(myApartmentsLink).toBeInTheDocument();
-  // expect(addAListingLink).toBeInTheDocument();
+
 
   const logOutLink = screen.queryByText(/log out/i);
   expect(logOutLink).toBeNull();
 });
-
-// test("renders Log Out link", () => {
-//   render(
-//     <BrowserRouter>
-//       <Header currentUser="someUser" />
-//     </BrowserRouter>
-//   );
-
-//   const myApartmentsLink = screen.getByText(/my apartments/i);
-//   const addAListingLink = screen.getByText(/add a listing/i);
-//   const logOutLink = screen.getByText(/log out/i);
-
-//   expect(myApartmentsLink).toBeInTheDocument();
-//   expect(addAListingLink).toBeInTheDocument();
-//   expect(logOutLink).toBeInTheDocument();
-// });
