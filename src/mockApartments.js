eece0a129522562c1c9e117c69790b3205c6fecf
Yoshihiro Yamada <yoshihiro.yamada1995@gmail.com>
2024-02-22 10:49:47 -0800

    finished apartment index with testing

--- src/mockApartments.js
+++ src/mockApartments.js
@@ -30,7 +30,7 @@ let mockApartments = [
     user_id: 1
   },
   {
-    id: 2,
+    id: 3,
     street: "415 Rainbow Street",
     unit: "3B",
     city: "San Diego",
@@ -42,7 +42,7 @@ let mockApartments = [
     pets: "small pets okay",
     image:
       "https://images.unsplash.com/photo-1469022563428-aa04fef9f5a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80",
-    user_id: 2
+    user_id: 3
   }
 ]
 
